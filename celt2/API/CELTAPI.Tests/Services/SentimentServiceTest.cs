a4244903940660c09419f495e45a9d9a03cbd8c6
Setu Kumar Basak <setu.1421@yahoo.com>
2021-09-14 00:13:12 -0400

    CEL-13: Added TestProject with NUnit and Moq

---
+++ celt2/API/CELTAPI.Tests/Services/SentimentServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using CELTAPI.Models;
+using CELTAPI.Services;
+using Moq;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace CELTAPI.Tests.Services
+{
+    /// <summary>
+    /// Tests for Sentiment Service
+    /// </summary>
+
+    public class SentimentServiceShould
+    {
+        [Test]
+        public async Task CalculateSentimentEquality()
+        {
+            var mockTextInput = new Mock<TextInput>();
+            var sut = new SentimentService();
+
+            var result = await sut.CalculateSentiment(mockTextInput.Object);
+
+            Assert.That(result, Is.EqualTo("positive"));
+        }
+    }
+}

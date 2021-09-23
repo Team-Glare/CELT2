631659af858b0a9ac41d1b316ec5b7278ed02182
ConorThomason <conorthomason@gmail.com>
2021-09-22 21:06:06 -0400

    Added dummy URL

--- celt2/server/app.py
+++ celt2/server/app.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask
+from flask import request
 # Boilerplate used from: https://flask.palletsprojects.com/en/2.0.x/quickstart/
 
 # Define the Flask App
@@ -10,6 +11,11 @@ app = Flask(__name__)
 def hello_world():
     # Returns the entirety of the page; in this case, it is just the word "Test"
     return "<p>Test</p>"
+
+@app.route("/submit_string")
+def accept_string():
+    data = request.get_json()
+    return "JSON: " + str(data) + " accepted
 
 if __name__ == '__main__':
     app.run()

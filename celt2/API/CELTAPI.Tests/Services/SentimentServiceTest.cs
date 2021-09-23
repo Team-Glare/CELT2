28524bb67cbead378ba7dfc9a2fef20c51d9df77
Setu Kumar Basak <sbasak4@ncsu.edu>
2021-09-23 13:46:20 -0400

    Fixed service test

--- celt2/API/CELTAPI.Tests/Services/SentimentServiceTest.cs
+++ celt2/API/CELTAPI.Tests/Services/SentimentServiceTest.cs
@@ -1,8 +1,11 @@
 ﻿using CELTAPI.Model;
 using CELTAPI.Services;
 using CELTAPI.Utilities;
+using Microsoft.AspNetCore.Http;
 using Moq;
 using NUnit.Framework;
+using System.IO;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace CELTAPI.Tests.Services
@@ -25,6 +28,28 @@ namespace CELTAPI.Tests.Services
 
             var sut = new SentimentService(mockStreamReader.Object, mockServerClient.Object);
             var result = await sut.CalculateSentimentFromText(mockTextInput.Object);
+
+            Assert.That(result, Is.EqualTo("Positive"));
+        }
+
+        [Test]
+        public async Task CalculateSentimentFromTextFileEquality()
+        {
+            var mockFormFile = new Mock<IFormFile>();
+            var mockStreamReader = new Mock<IStreamReader>();
+            var mockServerClient = new Mock<IServerClient>();
+
+            var mockResult = new SentimentResult
+            {
+                label = "Positive"
+            };
+
+            mockStreamReader.Setup(o => o.GetReader(mockFormFile.Object.OpenReadStream())).Returns(new StreamReader(new MemoryStream(Encoding.UTF8.GetBytes("This is a dummy file"))));
+
+            mockServerClient.Setup(o => o.PostAsync<SentimentResult, string>(It.IsAny<string>(), It.IsAny<string>())).ReturnsAsync(mockResult);
+
+            var sut = new SentimentService(mockStreamReader.Object, mockServerClient.Object);
+            var result = await sut.CalculateSentimentFromTextFile(mockFormFile.Object);
 
             Assert.That(result, Is.EqualTo("Positive"));
         }

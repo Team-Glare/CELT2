bc3a1aabd65bf114676fd8d5819a8502cfc43b2e
ConorThomason <conorthomason@gmail.com>
2021-09-27 20:56:50 -0400

    Run.sh created, added .NET install capability

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 pip install -r ./celt2/server/requirements.txt
 pip install -r ./celt2/model/requirements.txt -f https://download.pytorch.org/whl/lts/1.8/torch_lts.html
+./celt2/api/setup.sh

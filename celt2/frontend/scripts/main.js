34d42f22a5358daf0ce037e5723b7df4129fed6e
mjsohack <mjsohack@ncsu.edu>
2021-09-28 18:21:12 -0400

    Added documentation to JS

--- celt2/frontend/scripts/main.js
+++ celt2/frontend/scripts/main.js
@@ -1,17 +1,41 @@
-var apiBaseUrl = 'https://celtapi.azurewebsites.net/';
+/**
+ * Handles all frontend functionality for the Sentiment Analyzer.
+ *
+ * Retrieves and validates input and sends it to be parsed by the server and
+ * analyzed by the sentiment analyzer.
+ * Displays results that are returned after analysis. Displays an error message
+ * if analysis returns a result.
+ *
+ * @file   This files handles all frontend functionality for the Sentiment Analyzer
+ * @author Matthew Sohacki
+ * @since  1.0.1
+ */
 
+/* URL to send API requests to */
+var apiBaseUrl = "https://celtapi.azurewebsites.net/";
+
+/**
+ * Takes the file from the id='file' input and sends it
+ * to the file API to be be parsed by the server and analyzed
+ * by the sentiment analyzer.
+ * 
+ * If there is no file in the input, a popup is displayed telling the user
+ * to upload a file, and nothing is sent to be analyzed.
+ * 
+ * Displays the loading graphic while waiting for results to be returned.
+ * The results of analysis or an error message is displayed, depending on what
+ * the API returns.
+ *
+ */
 function analyzeFile() {
 
     var file = document.getElementById("file");
-    if (file.files.length == 0) {
-        window.alert("Choose file to analyze");
-        return;
-    }
+    if (!validateInput("file", file)) return;
 
     loadingResults();
 
     var formData = new FormData();
-    formData.append('file', file.files[0]);
+    formData.append("file", file.files[0]);
 
     $.ajax({
         type: "POST",
@@ -32,13 +56,23 @@ function analyzeFile() {
     });
 }
 
+/**
+ * Takes the text from the id='text' input and sends it
+ * to the text API to be be parsed by the server and analyzed
+ * by the sentiment analyzer.
+ * 
+ * If there is no text in the input, a popup is displayed telling the user
+ * to paste text in the input, and nothing is sent to be analyzed.
+ * 
+ * Displays the loading graphic while waiting for results to be returned.
+ * The results of analysis or an error message is displayed, depending on what
+ * the API returns.
+ *
+ */
 function analyzeText() {
 
     var text = document.getElementById("text");
-    if (text.value.length == 0) {
-        window.alert("Enter text to analyze");
-        return;
-    }
+    if (!validateInput("text", text)) return;
 
     loadingResults();
 
@@ -61,17 +95,47 @@ function analyzeText() {
     });
 }
 
+
+/**
+ * Checks that the given input contains a value and is not empty.
+ * If the input is empty a popup is displayed telling the user to enter input.
+ * 
+ * @param  {String}  inputType the input type being analyzed (i.e. file, text)
+ * @param            input the input to validate
+ *  @return {Boolean} true if the input is not empty, false if it is
+ */
+function validateInput(inputType, input) {
+    if (input.value.length == 0) {
+        window.alert("Enter " + inputType + " to analyze");
+        return false;
+    }
+    return true;
+}
+
+/**
+ * Results enter a "loading" state. The results output is cleared,
+ * the results output is hidden, and the loading graphic is displayed.
+ * 
+ */
 function loadingResults() {
     document.getElementById("result").text = "";
     document.getElementById("results").style.display = "none";
     document.getElementById("loader").style.display = "block";
 }
 
+/**
+ * Results are removed from a "loading" state. The results output is shown
+ * and the loading graphic is hidden.
+ * 
+ */
 function loadedResults() {
     document.getElementById("loader").style.display = "none";
     document.getElementById("results").style.display = "block";
 }
 
+/**
+ * The file input id='file' is cleared.
+ */
 function clearFile() {
     var file = document.getElementById("file");
     file.value = null;

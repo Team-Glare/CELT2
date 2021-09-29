03f6facc27047f8d175f706c0fd645a168fb5962
Setu Kumar Basak <sbasak4@ncsu.edu>
2021-09-28 22:53:49 -0400

    Added license text in the source files of API

--- celt2/API/CELTAPI/Startup.cs
+++ celt2/API/CELTAPI/Startup.cs
@@ -1,3 +1,11 @@
+/**
+* @license
+* Copyright Team Glare. All Rights Reserved.
+*
+* Use of this source code is governed by an MIT-style license that can be
+* found in the LICENSE file at https://github.com/Team-Glare/CELT2/blob/main/LICENSE
+*/
+
 using CELTAPI.Model;
 using CELTAPI.Services;
 using CELTAPI.Utilities;
--- celt2/API/CELTAPI/Utilities/IServerClient.cs
+++ celt2/API/CELTAPI/Utilities/IServerClient.cs
@@ -1,4 +1,12 @@
-﻿using System.Threading.Tasks;
+﻿/**
+* @license
+* Copyright Team Glare. All Rights Reserved.
+*
+* Use of this source code is governed by an MIT-style license that can be
+* found in the LICENSE file at https://github.com/Team-Glare/CELT2/blob/main/LICENSE
+*/
+
+using System.Threading.Tasks;
 
 namespace CELTAPI.Utilities
 {

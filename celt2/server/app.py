460c17146a33d0b07273cec4c2db75194adb3414
ConorThomason <conorthomason@gmail.com>
2021-09-30 01:42:34 -0400

    Added comments to app.py

--- celt2/server/app.py
+++ celt2/server/app.py
@@ -1,3 +1,10 @@
+#
+# @license
+# Copyright Team Glare. All Rights Reserved.
+#
+# Use of this source code is governed by an MIT-style license that can be
+# found in the LICENSE file at https://github.com/Team-Glare/CELT2/blob/main/LICENSE
+#
 import os
 import sys
 from flask import Flask
@@ -6,51 +13,59 @@ import json
 import random
 import nltk.data
 import subprocess
+
 # Allows for access to model. The Dockerfile accounts for this.
-print(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + 'model')
 try:
     from model.model import classify
 except ModuleNotFoundError:
     from model import classify
 # Boilerplate used from: https://flask.palletsprojects.com/en/2.0.x/quickstart/
-# Define the Flask App
+# Download the tokenizer. Wasn't used in Phase 1, but can be used in the future
+# for tokenizing sentences and doing a sentence-by-sentence sentiment analysis
 nltk.download('punkt', quiet=True)
+# Defines the aforementioned tokenizer
 tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+
+# Create_app returns a functioning Flask server with all of the route
+# definitions already included.
+# This is also necessary for pytest
+
 def create_app():
+    # Create app with the filename.
     app = Flask(__name__)
     # Define route, "/" specifies the landing page.
     @app.route("/", methods=['GET'])
     def hello_world():
-        # Returns the entirety of the page; in this case, it is just the word "Test"
+        # Returns "Hello world!". If you were to call this in a browser,
+        # you would see "Hello world!" appear likely in the top left
         return "Hello world!"
 
+    # Route for calling the model.
     @app.route("/sentiment/text", methods = ['POST'])
     def get_sentiment():
-        print("Received request: " + str(request))
-        model_path = "../model/model.py"
+        # Parse the json from the request received
         passed_data = request.get_json()['sentimentText']
-        print("Parsed data: " + str(passed_data))
-        # parsed_sentences = tokenizer.tokenize(passed_data)
-        # print("Tokenized sentences: " + str(parsed_sentences))
-        print("Attempting to get sentiments from sentences...")
 
+        # All sentiments and the commented-out code was implemented
+        # for multiple sentences. As Phase 1 was decided to only use
+        # single-sentence parsing, this will remain here until
+        # the other components of the architecture are capable of
+        # handling it.
         all_sentiments = []
-        # Currently, multiple sentences aren't supported by the API.
-        # The server can implement this with the following:
         # for sentence in parsed_sentences:
             # out = classify(sentence)
             # all_sentiments.append(str(out))
 
         # For now, we will be using a single sentence:
         sentence = passed_data
+        # Pass the sentence to the model
         out = classify(sentence)
         # Reassigning all_sentiments here just to avoid having to rename
         # later variables
         all_sentiments = out
-
-        print("Success, returning: " + str(all_sentiments))
         return json.dumps({'label': str(all_sentiments)})
+
     return app
 app = create_app()
 

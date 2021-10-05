cf2f58095e55daf0a10da31020dd698ae1c27843
ConorThomason <conorthomason@gmail.com>
2021-10-05 15:40:26 -0400

    Added privilege change to api setup

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 pip install -r ./celt2/server/requirements.txt
 pip install -r ./celt2/model/requirements.txt -f https://download.pytorch.org/whl/lts/1.8/torch_lts.html
+chmod +x ./celt2/API/setup.sh
 ./celt2/API/setup.sh
